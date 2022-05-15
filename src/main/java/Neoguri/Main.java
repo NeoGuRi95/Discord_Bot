1b3e27a327b5fa77c6e8f6c428aab3bd95cc3477
Jeahyun Park <jea5158@gmail.com>
2022-05-15 23:34:10 +0900

    first commit

--- src/main/java/Neoguri/Main.java
+++ src/main/java/Neoguri/Main.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.util.Random;
 
 public class Main extends ListenerAdapter {
     public static JDA jda;
@@ -16,7 +17,7 @@ public class Main extends ListenerAdapter {
 
         jda = JDABuilder.createDefault("OTc1MzcwOTQyMDYwNTkzMTUz.Grx5fw.39YeSNiThuknVdf2klMF2uP05m1Dxk1RhGrsoU").build();
         jda.getPresence().setStatus(OnlineStatus.ONLINE);
-        jda.getPresence().setActivity(Activity.playing("hi!"));
+        jda.getPresence().setActivity(Activity.playing("규민이랑 "));
 
         jda.addEventListener(new Main());
     }
@@ -25,16 +26,41 @@ public class Main extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
         String[] args = event.getMessage().getContentRaw().split(" ");
         String prefix = "!";
+
         if (!event.getAuthor().isBot()){
+            //hi
             if (args[0].equals(prefix + "hi")) {
                 //if (args.length == 1) event.getChannel().sendMessage("안녕").queue();
                 if (args.length == 1) event.getChannel().sendMessage("hello world!").queue();
             }
-            if (args[0].equals(prefix + "박규민")) {
-                if (args.length == 1) event.getChannel().sendMessage("나이: 28\n직업: 무직").queue();
+            //member
+            else if (args[0].equals(prefix + "박규민")) {
+                if (args.length == 1) event.getChannel().sendMessage("나이: 28\n일간 베스트 회원").queue();
             }
-            if (args[0].equals(prefix + "박시현")) {
-                if (args.length == 1) event.getChannel().sendMessage("나이: 28\n몸무게: 91KG\n블랙피그 원툴").queue();
+            else if (args[0].equals(prefix + "박시현")) {
+                if (args.length == 1) event.getChannel().sendMessage("나이: 28\n몸무게: 91KG").queue();
+            }
+            else if (args[0].equals(prefix + "한상우")) {
+                if (args.length == 1) event.getChannel().sendMessage("나이: 28\n자동차: 아반떼(사고차)").queue();
+            }
+            else if (args[0].equals(prefix + "차유민")) {
+                if (args.length == 1) event.getChannel().sendMessage("나이: 27\n???: 7cm").queue();
+            }
+            //random menu
+            else if (args[0].equals(prefix + "메뉴")) {
+                Random random = new Random();
+                random.setSeed(System.currentTimeMillis());
+
+                switch (random.nextInt(3)) {
+                    case 0: event.getChannel().sendMessage("항아리 족발").queue();
+                            break;
+                    case 1: event.getChannel().sendMessage("블랙피그").queue();
+                            break;
+                    case 2: event.getChannel().sendMessage("앗싸곱창").queue();
+                            break;
+                    case 3: event.getChannel().sendMessage("앗싸곱창(상우차에서)").queue();
+                            break;
+                }
             }
         }
     }
